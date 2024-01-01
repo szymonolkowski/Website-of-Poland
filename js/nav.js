fc4c1f07192a46b16df01f875076cb544ca7a6da
Szymon D. Olkowski <114610987+szymonolkowski@users.noreply.github.com>
2024-01-01 22:02:40 +0100

    navbar completed

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,40 @@
-let nav_ele = document.querySelectorAll("#navbar li");
+let nav_ele_a = document.querySelectorAll("#navbar a");
+let info_bubble = document.querySelector("#info-navbar");
+let info_bubble_text = document.querySelector("#info-navbar #inside-info-navbar");
 
-nav_ele.forEach(e => {
-	e.addEventListener("mouseover", e => {
+document.addEventListener("mousemove", function (e) {
+	let mouseX = e.clientX;
+	let mouseY = e.clientY;
 
+	let follower = info_bubble;
+
+	// Use vw units for positioning
+	follower.style.left =
+		((mouseX - follower.offsetWidth) / window.innerWidth) * 100-0.9 + "vw";
+	follower.style.top = (mouseY / window.innerHeight) * 100 + 6 + "vh";
+});
+
+nav_ele_a.forEach((e) => {
+	let navbar = document.querySelector("#navbar ul");
+	navbar.addEventListener("mouseover", (e) => {
+		let id_of_li = e.target.id;
+		let dashIndex = id_of_li.indexOf('-');
+		let resultString = dashIndex !== -1 ? id_of_li.substring(0, dashIndex) : id_of_li;
+		if (resultString == "home"){
+			info_bubble_text.innerHTML = "Strona Główna";
+		}else if (resultString == "pns"){
+			info_bubble_text.innerHTML = "Polskie Symbole Narodowe";
+		}else if (resultString == "geo"){
+			info_bubble_text.innerHTML = "Geografia i Miasta Polski";
+		}else if (resultString == "law"){
+			info_bubble_text.innerHTML = "Władza w Polsce";
+		}else if (resultString == "me"){
+			info_bubble_text.innerHTML = "O mnie";
+		}
+
+		info_bubble.style.opacity = "1";
+	});
+	navbar.addEventListener("mouseout", (e) => {
+		info_bubble.style.opacity = "0";
 	});
 });
