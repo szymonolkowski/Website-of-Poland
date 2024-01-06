f8235f8174e298e73d82c0e27eb0e56f6f607a50
Szymon D. Olkowski <114610987+szymonolkowski@users.noreply.github.com>
2024-01-06 15:54:53 +0100

    random changes

--- js/nav.js
+++ js/nav.js
@@ -10,9 +10,8 @@ document.addEventListener("mousemove", function (e) {
 
 	let follower = info_bubble;
 
-	// Use vw units for positioning
 	follower.style.left =
-		(mouseX /* - follower.offsetWidth */ / window.innerWidth) * 100 -
+		(mouseX / window.innerWidth) * 100 -
 		0.9 +
 		"vw";
 	follower.style.top =
@@ -38,10 +37,10 @@ nav_ele_a.forEach((e) => {
 			info_bubble_text.innerHTML = "O mnie";
 		}
 		info_bubble.classList.remove("hide");
-		info_bubble.classList.add("show"); // Add a class to trigger animation
+		info_bubble.classList.add("show"); 
 	});
 	navbar.addEventListener("mouseout", (e) => {
-		info_bubble.classList.remove("show"); // Remove the class to hide the animation
+		info_bubble.classList.remove("show"); 
 		info_bubble.classList.add("hide");
 	});
 });
---
+++ test/main.js
@@ -0,0 +1,73 @@
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+};
+
+const texts = [
+    "If",
+    "You",
+    "Like",
+    "It",
+    "Please",
+    "Give",
+    "a Love",
+    ":)",
+    "by @DotOnion"
+];
+
+const morphTime = 1;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph = 0; // Reset morph instead of subtracting cooldown
+
+    let fraction = morph / time+1;
+
+    if (fraction > 1) {
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let dt = (newTime - time) / 100000;
+    time = newTime;
+
+    // Remove cooldown-related logic and directly increment textIndex
+    textIndex++;
+    doMorph();
+}
+
+animate();
